17f72e07a895d2976bc422405a137d0701122045
xennia316 <xennia316@gmail.com>
2021-12-30 19:54:08 +0100

    Print hello world

---
+++ Core_Java_Programming/program_01.java
@@ -0,0 +1,13 @@
+public class MyFirstJavaProgram
+{
+/**
+ * This is my first program in JAVA.
+ * By @xennia316
+ */
+
+ public static void main(string[] args)
+ {
+     System.out.println("Hello World");
+
+ }
+}
