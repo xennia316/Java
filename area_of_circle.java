67b16a320e8d45febd73a52b77eeb8c3c4be1ec0
xennia316 <xennia316@gmail.com>
2022-01-10 22:45:01 +0100

    finding the area off a circle with scanned radius

---
+++ area_of_circle.java
@@ -0,0 +1,20 @@
+/**
+ * Scanning the radius of a circle and finding its corresponding area.
+ * by xennia316@github.com
+ */
+
+import java.util.Scanner;
+
+ public class  AreaOfcircle
+ {
+    public class void main(string arg[])
+    {
+        scanner s = new Scanner(System.in);
+
+        System.out.println("Enter radius of the circle: ");
+        double r = s.nextDouble();
+        double area = (22 * r * r) / 7;
+
+        System.out.println("Area of circle is: "+area);
+    }
+ }
