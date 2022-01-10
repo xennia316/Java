0b9d17316777052aebb02d2f56978ec6f48cfc50
xennia316 <xennia316@gmail.com>
2022-01-10 22:28:59 +0100

    using scanner and summing two numbers

---
+++ Core_Java_Programming/add_two_numbers2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class AddTwoNumbers2
+{
+    public stsatic void main(string[] args)
+    {
+        int num1, num2, sum;
+        Scanner sc = new Scanner(System .in);
+
+        System.out.println("Enter first number: ");
+        num1 = sc.nextInt();
+
+        System.out,println("Enter second number: ");
+        num2 = sc. nextInt();
+
+        sc.close();
+
+        sum = num1 + num2;
+
+        System.out.println("sum of these numbers: "+sum);
+        
+    }
+}
