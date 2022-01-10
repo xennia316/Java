7082c9097b44f6c9d82c232dd55fcc3264cd8ee2
xennia316 <xennia316@gmail.com>
2022-01-10 22:21:39 +0100

    adding two numbers

---
+++ Core_Java_Programming/add_two_numbers.java
@@ -0,0 +1,16 @@
+/**
+ * Sum of two numbers by @xennia316
+ */
+
+ public class AddTwoNumbers
+ {
+     public static void main(string[] args)
+     {
+
+     int num1 = 5, num2 = 15, sum;
+
+     sum = num1 + num2;
+
+     System.out.println("Sum of these numbers: "+sum);
+    }
+ }
